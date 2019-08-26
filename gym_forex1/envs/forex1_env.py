1488d9ba691fc808883071db9bb47f6b9cc5bf81
crilazar <49878304+crilazar@users.noreply.github.com>
2019-08-26 13:31:03 -0400

    data processing changes

--- gym_forex1/envs/forex1_env.py
+++ gym_forex1/envs/forex1_env.py
@@ -17,8 +17,8 @@ class Forex1(gym.Env):
 
         df = pd.read_csv('./data/GBPUSD_Jan_2019_to_Aug_2019.csv')
         df = df.sort_values('Date_time')
-        self.df = df
-        self.max_step = len(self.df.loc[:, 'Cycle_12_14_H4_1'].values) - 2
+        self.df = df.iloc[:, 1:].values
+        self.max_step = len(self.df) - 2
 
         self.CurrentMarketLevel = 0
         self.active_trade = 0
@@ -39,54 +39,13 @@ class Forex1(gym.Env):
 
     def _get_current_step_data(self):
         # Get the stock data points for the last 5 days and scale to between 0-1
-        data_current_step = np.array([
-            self.df.loc[self.current_step:self.current_step, 'Cycle_12_14_H4_1'].values,
-            self.df.loc[self.current_step:self.current_step, 'Cycle_12_14_H4_0'].values,
-            self.df.loc[self.current_step:self.current_step, 'CyH4_slope'].values,
-            self.df.loc[self.current_step:self.current_step, 'CycleH4age'].values,
-            self.df.loc[self.current_step:self.current_step, 'Cycle_12_14_H1_1'].values,
-            self.df.loc[self.current_step:self.current_step, 'Cycle_12_14_H1_0'].values,
-            self.df.loc[self.current_step:self.current_step, 'ColorStochastic_M5'].values,
-            self.df.loc[self.current_step:self.current_step, 'ColorStochastic_M15'].values,
-            self.df.loc[self.current_step:self.current_step, 'CyH1_slope'].values,
-            self.df.loc[self.current_step:self.current_step, 'CycleH1age'].values,
-            self.df.loc[self.current_step:self.current_step, 'Cycle_12_14_M15_1'].values,
-            self.df.loc[self.current_step:self.current_step, 'Cycle_12_14_M15_0'].values,
-            self.df.loc[self.current_step:self.current_step, 'CyM15_slope'].values,
-            self.df.loc[self.current_step:self.current_step, 'LagN_blue'].values,
-            self.df.loc[self.current_step:self.current_step, 'LagN_purple'].values,
-            self.df.loc[self.current_step:self.current_step, 'LagN_yellow'].values,
-            self.df.loc[self.current_step:self.current_step, 'LagN_lime'].values,
-            self.df.loc[self.current_step:self.current_step, 'LagN_orange'].values,
-            self.df.loc[self.current_step:self.current_step, 'Last_LagN_yellow'].values,
-            self.df.loc[self.current_step:self.current_step, 'Last_LagN_lime'].values,
-            self.df.loc[self.current_step:self.current_step, 'Last_LagN_orange'].values,
-            self.df.loc[self.current_step:self.current_step, 'LagN_yellow_extreme'].values,
-            self.df.loc[self.current_step:self.current_step, 'LagN_lime_extreme'].values,
-            self.df.loc[self.current_step:self.current_step, 'EMA_blue_M15'].values,
-            self.df.loc[self.current_step:self.current_step, 'EMA_red_M15'].values,
-            self.df.loc[self.current_step:self.current_step, 'EMA_red_M15_slope'].values,
-            self.df.loc[self.current_step:self.current_step, 'EMA_trend_M15'].values,
-            self.df.loc[self.current_step:self.current_step, 'EMAage'].values,
-            self.df.loc[self.current_step:self.current_step, 'EMAdelta'].values,
-            self.df.loc[self.current_step:self.current_step, 'Market_to_EMA_blue'].values,
-            self.df.loc[self.current_step:self.current_step, 'CurrentMarketLevel'].values,
-            self.df.loc[self.current_step:self.current_step, 'EMA_red_H1'].values,
-            self.df.loc[self.current_step:self.current_step, 'EMA_blue_H1'].values,
-            self.df.loc[self.current_step:self.current_step, 'EMA_trend_H1'].values,
-            self.df.loc[self.current_step:self.current_step, 'EMAdelta_H1'].values,
-            self.df.loc[self.current_step:self.current_step, 'EMAage_H1'].values,
-            self.df.loc[self.current_step:self.current_step, 'EMA_red_H1_slope'].values,
-            self.df.loc[self.current_step:self.current_step, 'Market_to_EMA_blue_H1'].values,
-        ])
+        data_current_step = self.df[self.current_step]
         self.CurrentMarketLevel = data_current_step[30]
         output_data = np.append(data_current_step, [[
                     self.active_trade,
                     float(self.profit)
                 ]])
-
         obs = self._normalize_data(output_data)
-
         return obs
 
     def _normalize_data(self, input_data):
