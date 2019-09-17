05268b89e8df787545e40861bb80ad24a8b76597
crilazar <49878304+crilazar@users.noreply.github.com>
2019-09-17 08:55:26 -0400

    Update forex1_env.py

--- gym_forex1/envs/forex1_env.py
+++ gym_forex1/envs/forex1_env.py
@@ -191,7 +191,7 @@ class Forex1(gym.Env):
         if self.close_profit > 150:
             reward = self.close_profit + self.last_trade_length / 50 + 10
             self.close_profit = 0
-        if self.close_profit > 100:
+        elif self.close_profit > 100:
             reward = self.close_profit + self.last_trade_length / 50 + 8
             self.close_profit = 0                        
         elif self.close_profit > 60:
@@ -206,7 +206,7 @@ class Forex1(gym.Env):
         elif self.close_profit > 5:
             if self.last_trade_length > 10:
                 reward = self.close_profit
-            else:
+            if self.last_trade_length < 10:
                 reward = self.close_profit / 10
             self.close_profit = 0
 
